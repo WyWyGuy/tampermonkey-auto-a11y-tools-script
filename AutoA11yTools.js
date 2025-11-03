c5c11da2b8cada0427b1581011c5e09597980256
WyWyGuy <64562729+WyWyGuy@users.noreply.github.com>
2025-11-03 14:28:28 -0700

    Update and rename AutoA11yTools.user.js to AutoA11yTools.js

--- AutoA11yTools.js
+++ AutoA11yTools.js
@@ -10,8 +10,8 @@
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_unregisterMenuCommand
-// @updateURL    https://raw.githubusercontent.com/WyWyGuy/tampermonkey-auto-a11y-tools-script/main/AutoA11yTools.user.js
-// @downloadURL  https://raw.githubusercontent.com/WyWyGuy/tampermonkey-auto-a11y-tools-script/main/AutoA11yTools.user.js
+// @updateURL    https://raw.githubusercontent.com/WyWyGuy/tampermonkey-auto-a11y-tools-script/main/AutoA11yTools.js
+// @downloadURL  https://raw.githubusercontent.com/WyWyGuy/tampermonkey-auto-a11y-tools-script/main/AutoA11yTools.js
 // ==/UserScript==
 
 (function () {
